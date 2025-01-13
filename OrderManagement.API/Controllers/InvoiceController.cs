8c95f978a33130ac3482d79b30f601b1805e0809
MihaiFilca <120052866+misu1403@users.noreply.github.com>
2025-01-13 23:10:44 +0200

    Invoice_API

---
+++ OrderManagement.API/Controllers/InvoiceController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderManagement.Application.DTOs;
+using OrderManagement.Application.Services;
+
+[ApiController]
+[Route("api/[controller]")]
+public class InvoicesController : ControllerBase
+{
+    private readonly InvoiceService _invoiceService;
+
+    public InvoicesController(InvoiceService invoiceService)
+    {
+        _invoiceService = invoiceService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateInvoice(CreateInvoiceDto dto)
+    {
+        var invoice = await _invoiceService.CreateInvoiceAsync(dto);
+        return CreatedAtAction(nameof(GetInvoice), new { id = invoice.InvoiceId }, invoice);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetInvoice(Guid id)
+    {
+        try
+        {
+            var invoice = await _invoiceService.GetInvoiceAsync(id);
+            return Ok(invoice);
+        }
+        catch (InvalidOperationException ex)
+        {
+            return NotFound(new { Message = ex.Message });
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllInvoices()
+    {
+        var invoices = await _invoiceService.GetAllInvoicesAsync();
+        return Ok(invoices);
+    }
+
+
+
+
+}
